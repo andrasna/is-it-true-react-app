cee74da73e3eb500786b8f30b63037b34e6af2b4
Andras Nagy <contact@andras.me>
2022-01-20 00:20:27 +0100

    fetch some content

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,17 @@
 import './App.css'
+import { useEffect } from 'react'
 
 function App() {
+  useEffect(() => {
+    ;(async () => {
+      const url =
+        'https://opentdb.com/api.php?amount=10&difficulty=hard&type=boolean'
+      const response = await fetch(url)
+      const { results } = await response.json()
+      console.log(results)
+    })()
+  }, [])
+
   return (
     <div className="App">
       <h1>Hello World</h1>
