030ea0100d6da493ce6747525c41ff52f9b96b1c
Andras Nagy <contact@andras.me>
2022-01-20 16:26:13 +0100

    Add quiz html

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,14 +2,14 @@ import { Link } from 'react-router-dom'
 
 function Home() {
   return (
-    <div>
+    <main>
       <h1>Welcome to the Trivia Challenge!</h1>
 
-      <p>You will be presented with 10 true of false questions.</p>
+      <p>You will be presented with 4 true of false questions.</p>
       <p>Can you score 100%?</p>
 
       <Link to="/quiz">Begin</Link>
-    </div>
+    </main>
   )
 }
 
--- src/pages/Quiz.js
+++ src/pages/Quiz.js
@@ -1,5 +1,28 @@
 function Quiz() {
-  return <h1>The Quiz</h1>
+  return (
+    <main>
+      <p>Category: lorem ipsum</p>
+
+      <p>Question 1 / 4</p>
+
+      <p>
+        The fourth funnel of the RMS Titanic was fake designed to make the ship
+        look more powerful and symmetrical.
+      </p>
+
+      <div>
+        <input type="radio" id="true" name="answer" value="true" />
+        <label htmlFor="true">True</label>
+      </div>
+
+      <div>
+        <input type="radio" id="false" name="answer" value="false" />
+        <label htmlFor="false">False</label>
+      </div>
+
+      <button>Next</button>
+    </main>
+  )
 }
 
 export default Quiz
